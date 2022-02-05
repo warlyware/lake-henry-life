405cc900a806b0f6e9dab7aefe3324034dd193af
Dan Ward <danward@gmail.com>
2022-02-05 01:10:28 -0800

    Add credit line

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,31 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="w-full h-full flex justify-center items-center relative">
-    <div class="h-full w-full absolute top-0 left-0 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
-    <div class="h-full w-full absolute top-0 left-0 lake-bg bg-center"></div>
-    <h1 
+  <div class="h-full w-full absolute top-0 left-0 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+  <div class="h-full w-full absolute top-0 left-0 lake-bg bg-center"></div>
+  <div class="w-full h-full min-h-screen flex flex-col justify-center items-center relative">
+    <div 
       class="text-center text-yellow-400 max-w-3xl z-10 font-bold bg-blue-900 py-4 rounded-md"
       style="background-color: rgba(30, 58, 175, 0.4)"
     >
       <h1 class="md:text-9xl text-6xl tracking-wide">
         Life is better on the lake
       </h1>
-    </h1>
+    </div>
+  </div>
+  <div class="absolute bottom-2 z-10 text-white mx-auto w-full text-center text-md font-bold uppercase">
+    Made with ♥️ by
+    <a href="https://twitter.com/warly_sol" 
+    target="_blank"
+    style="background-color: rgba(30, 58, 175, 0.4)"
+    >
+      warly.sol
+    </a>
   </div>
 </template>
 
 <style>
-#app {
+h1 {
   @apply h-full;
   font-family: 'Dancing Script', cursive;
   -webkit-font-smoothing: antialiased;
