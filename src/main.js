9b3f14a48f11f6d340a9a54ae621d54bfd8899fa
Dan Ward <danward@gmail.com>
2022-02-05 15:45:59 -0800

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
+import VueGtag from 'vue-gtag-next'
+
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueGtag, {
+  config: { 
+    id: 'G-9MRSBG5BWV',
+  },
+})
+
+app.mount('#app')
